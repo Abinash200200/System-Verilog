aafc2258bf3f9be1ccfddbecd3d8756094253dd0
Abinash200200 <160818690+Abinash200200@users.noreply.github.com>
2024-05-02 10:47:20 +0530

    Static_Method.sv

--- Class/Static_Method.sv
+++ Class/Static_Method.sv
@@ -1,1 +1,27 @@
+ class packet;
+   byte a;
+   static byte b;
+   
+   function new();
+     b++;
+     a=b;
+   endfunction
+   
+  static function void display();
+     $display("--------------------");
+     $display("Number of time =%0d",b);
+     $display("--------------------");
+  endfunction
+endclass
+ 
+ module pkt;
+   packet koo[4];
+   
+   initial begin
+    foreach(koo[i])begin
+      koo[i]=new();
+     end
+     koo[0].display();
+   end 
+endmodule
 
