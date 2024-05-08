0d16fe4feb6f09963945204fc488622a312705fc
Abinash200200 <160818690+Abinash200200@users.noreply.github.com>
2024-05-08 13:35:03 +0530

    $urandom.sv

--- Randomization_and_Constraints/$urandom.sv
+++ Randomization_and_Constraints/$urandom.sv
@@ -1,1 +1,14 @@
+module system_funcations;
+  bit [31:0] addr1;
+  bit [31:0] addr2;
+  bit [64:0] addr3;
+  bit [31:0] data;
+  initial begin
+    addr1 = $urandom();
+    addr2 = $urandom(89);
+    addr3 = {$urandom(),$urandom()};
+    data  = $urandom * 6;
 
+    $display("addr1=%0d, addr2=%0d, addr3=%0d, data=%0d",addr1,addr2,addr3,data);
+  end
+endmodule
