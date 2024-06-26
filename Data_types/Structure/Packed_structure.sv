9b4a5cc21aa43d3ae3062f55b06639b9b57f9d44
Abinash200200 <160818690+Abinash200200@users.noreply.github.com>
2024-06-26 12:47:30 +0530

    Packed_structure.sv

--- Data_types/Structure/Packed_structure.sv
+++ Data_types/Structure/Packed_structure.sv
@@ -1,1 +1,22 @@
-ijj
+module struct_packed; // Module declaration
+  
+  typedef struct packed { // Declaration of a packed struct
+    bit[7:0] Student; // 8-bit field for Student
+    int Number_Student; // Integer field for Number_Student
+    longint Fee; // Long integer field for Fee
+  } Student_details; //  Name of the struct type
+  
+  Student_details std; // Declaration of a variable of type Student_details
+  
+  initial begin // Begin initial block
+    std.Student = 5; // Initialize Student field of std to 5
+    std.Number_Student = 3; // Initialize Number_Student field of std to 3
+    std.Fee = 40000; // Initialize Fee field of std to 40000
+    
+    $display("Student %p", std.Student); // Display value of Student field in std
+    $display("Number of Student %p", std.Number_Student); // Display value of Number_Student field in std
+    $display("Fee for the Student %p", std.Fee); // Display value of Fee field in std
+    $display("Bitstream of the Student %0d", $bits(std)); // Display the number of bits in std
+    
+  end // End initial block
+endmodule // End module
